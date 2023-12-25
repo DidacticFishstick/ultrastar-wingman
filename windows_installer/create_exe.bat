e5ecb0f84a21cf112d5b481d2b9f5235cad9a4c3
Didactic Fishstick <didactic-fishstick@proton.me>
2023-12-25 02:30:15 +0100

    Windows Installer

--- windows_installer/create_exe.bat
+++ windows_installer/create_exe.bat
@@ -1,1 +1,2 @@
-pyinstaller --onefile --hide-console hide-early --add-data "../avatars;./avatars" --add-data "../config.ini.windows_installation_template;./" --name=ultrastar-wingman --runtime-hook add_environment.py --icon=../static/icons/wingman.ico ../main.py
+pyinstaller --onefile --add-data "../avatars;./avatars" --add-data "executables;./executables" --add-data "../config.ini.windows_installation_template;./" --name=ultrastar-wingman --runtime-hook add_environment.py --icon=../static/icons/wingman.ico ../main.py
+"D:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc windows_installer.iss
