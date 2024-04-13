5b75c274fef0283cd0b5578f3796985946c6419b
Didactic Fishstick <didactic-fishstick@proton.me>
2024-04-13 23:55:47 +0200

    Set app to use proxy

--- ultrastar-wingman/frontend/src/App.js
+++ ultrastar-wingman/frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {ApiClient, PlayersApi} from "./api/src";
+import {ApiClient} from "./api/src";
 import BottomNav from './components/BottomNav';
 import Home from './components/Home';
 import Songs from './components/Songs';
@@ -7,17 +7,9 @@ import './App.css';
 
 
 const defaultClient = ApiClient.instance;
-defaultClient.basePath = "http://localhost:8080";
+defaultClient.basePath = window.location.origin;
 
 function App() {
-
-    const playersApi = new PlayersApi();
-    playersApi.apiPlayersApiPlayersGet((error, data, response) => {
-        console.log(error);
-        console.log(data);
-        console.log(response);
-    });
-
     return (
         <div>
             <BrowserRouter>
